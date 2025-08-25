82c2a9cbc95a049ab4fcdbf7a937cf95554c6997
ricardo-ervilha <ricardoervilhacomp@gmail.com>
2025-08-25 13:43:39 -0300

    Add benchmark

--- app/main.cpp
+++ app/main.cpp
@@ -9,8 +9,7 @@ double right(double x, double y) {
 }
 
 double top(double x, double y) {
-    // return sin(M_PI * x) * sin(M_PI);
-    return 0;
+    return 0.0;
 }
 
 double left(double x, double y) {
@@ -31,16 +30,15 @@ pair<double,double> U(double x, double y){
 
 double source(double x, double y){
     return -200*x*(1-x) - 200*y*(1-y);
-    // return 0;
 }
 
 double exact(double x, double y){
-    return 100*x*(1-x)*y*(1-y); // Exata do problema de Poisson.
+    return 100*x*(1-x)*y*(1-y); 
 }
 
 int main(void){
     Mesh* m = new Mesh();
-    m->read_mesh("../inputs/1026tri.msh");
+    m->read_mesh("../inputs/24x24.msh");
 
     BoundaryCondition* downBC = new BoundaryCondition(DIRICHLET, DOWN, down);
     BoundaryCondition* rightBC = new BoundaryCondition(DIRICHLET, RIGHT, right);
@@ -49,14 +47,11 @@ int main(void){
     FVMSolver* solver = new FVMSolver(m, downBC, rightBC, topBC, leftBC, gamma, rho, U, source);
 
     solver->assembly_A();
-    solver->print_A();
 
     solver->assembly_b();
-    solver->print_b();
 
     double tol = 1e-6;
     solver->solve_system(tol);
-    // solver->compute_error(exact);
 
     solver->save_solution("../outputs/result.vtk");
 
